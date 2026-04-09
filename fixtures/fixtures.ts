68c7a06943106d35c34448b9cd21f8b8eabc37a5
Touhidul islam <touhidulislam066@gmail.com>
2026-04-09 15:40:12 +0300

    Fixtures

---
+++ fixtures/fixtures.ts
@@ -0,0 +1,40 @@
+import { test as base } from '@playwright/test';
+import { LoginPage } from '../pages/Login';
+import { InventoryPage } from '../pages/Inventory';
+import { CartPage } from '../pages/Cart';
+import { CheckoutPage } from '../pages/Checkout';
+import { FinishPage } from '../pages/Finish';
+import { SidebarPage } from '../pages/sidbar';
+
+type MyFixtures = {
+    loginPage: LoginPage;
+    inventoryPage: InventoryPage;
+    cartPage: CartPage;
+    checkoutPage: CheckoutPage;
+    finishPage: FinishPage;
+    sidebarPage: SidebarPage;
+};
+
+// Extending the base test with our POM objects
+export const test = base.extend<MyFixtures>({
+    loginPage: async ({ page }, use) => {
+        await use(new LoginPage(page));
+    },
+    inventoryPage: async ({ page }, use) => {
+        await use(new InventoryPage(page));
+    },
+    cartPage: async ({ page }, use) => {
+        await use(new CartPage(page));
+    },
+    checkoutPage: async ({ page }, use) => {
+        await use(new CheckoutPage(page));
+    },
+    finishPage: async ({ page }, use) => {
+        await use(new FinishPage(page));
+    },
+    sidebarPage: async ({ page }, use) => {
+        await use(new SidebarPage(page));
+    },
+});
+
+export { expect } from '@playwright/test';
