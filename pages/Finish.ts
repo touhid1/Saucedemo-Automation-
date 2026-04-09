d2c44d1d0c69837a4f670812ce39d8bb1f65163c
Touhidul islam <touhidulislam066@gmail.com>
2026-04-09 15:37:49 +0300

    Finish page

---
+++ pages/Finish.ts
@@ -0,0 +1,20 @@
+import { Page, Locator, expect } from '@playwright/test';
+export class FinishPage {
+    private readonly page: Page;
+    private readonly completeHeader: Locator;
+    private readonly backHomeButton: Locator;
+
+    constructor(page: Page) {
+        this.page = page;
+        this.completeHeader = page.locator('.complete-header');
+        this.backHomeButton = page.locator('[data-test="back-to-products"]');
+    }
+
+    async assertSuccessMessage(expectedHeader: string) {
+        await expect(this.completeHeader).toHaveText(expectedHeader);
+    }
+
+    async backHome() {
+        await this.backHomeButton.click();
+    }
+}
