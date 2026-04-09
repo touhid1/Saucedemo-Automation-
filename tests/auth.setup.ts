a7ad319d035bc64d6ae8646efe736206748d9534
Touhidul islam <touhidulislam066@gmail.com>
2026-04-09 15:42:46 +0300

    auth setup

---
+++ tests/auth.setup.ts
@@ -0,0 +1,15 @@
+import { test as setup, expect } from '@playwright/test';
+import { LoginPage } from '../pages/Login';
+import { testData } from '../data/testData';
+
+const authFile = 'playwright/.auth/user.json';
+
+setup('authenticate', async ({ page }) => {
+    const loginPage = new LoginPage(page);
+    await loginPage.navigate();
+    await loginPage.login(testData.validUser.username, testData.validUser.password);
+
+    await expect(page).toHaveURL(/.*inventory.html/);
+
+    await page.context().storageState({ path: authFile });
+});
